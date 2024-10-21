e5e87c115c8b7141a7624f81333f6059fc90ebe6
Lewis <lewisryework@gmail.com>
2024-10-21 16:17:00 +0100

    Cleanup k8scale_custom.py

--- socialNetwork/k8scale_custom.py
+++ socialNetwork/k8scale_custom.py
@@ -1,6 +1,7 @@
-from kubernetes import client, config, watch
-import requests
-import time
+from kubernetes import client, config
+
+from kubernetes.client import ApiException
+
 
 def main():
     config.load_kube_config()
@@ -13,6 +14,7 @@ def main():
     disable_hpa("socialnetwork", core_api, autoscale_api)
     disable_replicas("socialnetwork", core_api, apps_api)
 
+
 def disable_hpa(namespace, core_api, autoscale_api):
     try:
         pods = core_api.list_namespaced_pod(namespace=namespace).items
@@ -22,18 +24,18 @@ def disable_hpa(namespace, core_api, autoscale_api):
             resource_name = '-'.join(pod.metadata.name.split('-')[:-2])
 
             try:
-                # disable HPA and add pod to disabled set
+                # disable HPA
                 autoscale_api.delete_namespaced_horizontal_pod_autoscaler(
                     name=resource_name,
                     namespace=namespace,
                 )
-                print(f"Ensure HPA Disabled: {resource_name}")
-            except Exception as e:
-                print(f"Ensure HPA Disabled: {resource_name}")
+                print(f"Disable HPA: {resource_name}")
+            except ApiException as e:
                 pass
 
     except Exception as e:
         print(f"Unexpected Exception: {e}")
+
 
 def disable_replicas(namespace, core_api, apps_api):
     try:
@@ -44,53 +46,29 @@ def disable_replicas(namespace, core_api, apps_api):
 
             try:
                 # remove all replicas
-                deployment = apps_api.read_namespaced_deployment(name=resource_name, namespace=namespace)
+                deployment = apps_api.read_namespaced_deployment(
+                    name=resource_name,
+                    namespace=namespace
+                )
                 deployment.spec.replicas = 1
                 # apply replica reduction
-                apps_api.patch_namespaced_deployment(name=resource_name, namespace=namespace, body=deployment)
-                print(f"Ensure Replica Removal: {resource_name}")
-            except Exception as e:
-                print(f"Ensure Replica Removal: {resource_name}")
+                apps_api.patch_namespaced_deployment(
+                    name=resource_name,
+                    namespace=namespace,
+                    body=deployment
+                )
+                print(f"Disable Replicas: {resource_name}")
+            except ApiException as e:
                 pass
 
     except Exception as e:
         print(e)
 
+
 if __name__ == "__main__":
     main()
 
-
-
-
-
-
-
-
-
-
-'''
-try:
-    config.load_kube_config()
-    core_api = client.CoreV1Api()
-    apps_api = client.AppsV1Api()
-    namespaces = core_api.list_namespace()
-    for namespace in namespaces.items:
-        namespace_name = namespace.metadata.name
-        pods = core_api.list_namespaced_pod(namespace=namespace_name, watch=False).items
-        if len(pods) > 0:
-            print(f"---{namespace_name}---")
-
-            for pod in pods:
-                pod_name = pod.metadata.name
-                print(pod_name, f"({pod.status.pod_ip})")
-
-            print("")
-except Exception as e:
-    print(e)
-    print("Ensure that Kubernetes is running before opening this script.")
-    quit()
-
-LIST SERVICES CAUGHT BY JAEGER
+''' keep for later - jaeger http endpoints
 http_response = requests.get("http://localhost:16686/api/services")
 services = http_response.json()
 for service in services["data"]:
