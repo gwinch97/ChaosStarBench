a008a3a36b34652ceaa46f49cf92006a259c0926
Lewis <lewisryework@gmail.com>
2024-10-24 22:09:54 +0100

    Edit dockerfiles

--- socialNetwork/k8startup.sh
+++ socialNetwork/k8startup.sh
@@ -22,7 +22,35 @@ minikube addons enable csi-hostpath-driver
 kubectl patch storageclass csi-hostpath-sc -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
 # Setup deployment
-bash setup-k8.sh
+if kubectl get namespace "socialnetwork" > /dev/null 2>&1; then
+	echo "Namespace socialnetwork exists."
+else
+	kubectl create namespace socialnetwork
+	kubectl config set-context --current --namespace=socialnetwork
+	cd helm-chart
+	helm install v1 socialnetwork
+fi
+
+if kubectl get namespace "monitoring" > /dev/null 2>&1; then
+	echo "Namespace monitoring exists."
+else
+	cd ../..
+	kubectl create namespace monitoring
+	kubectl config set-context --current --namespace=monitoring
+	helm install cadvisor ./cadvisor
+	helm install prometheus ./prometheus
+	kubectl create configmap jaeger-sampling-strategy --from-file=jaeger/sampling-strategy.json
+	kubectl config set-context --current --namespace=default
+	#helm install jaeger jaegertracing/jaeger -f jaeger/values.yaml
+fi
+
+if kubectl get namespace "chaos-mesh" > /dev/null 2>&1; then
+        echo "Namespace chaos-mesh exists."
+else
+	kubectl create namespace chaos-mesh
+	kubectl config set-context --current --namespace=chaos-mesh
+	helm install chaos-mesh ./chaos-mesh
+fi
 
 # Scale socialnetwork deployment
 if [ "$n_inst" -gt 1 ]; then
