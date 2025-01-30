13e5500d409b34ceffac06958a43d06eea3fa20f
Lewis <lewisryework@gmail.com>
2025-01-30 12:09:31 +0000

    Further work on autoscale algorithm

--- socialNetwork/k8scale_custom.py
+++ socialNetwork/k8scale_custom.py
@@ -1,5 +1,7 @@
 """This file runs an autoscaling algorithm for the Social Network Kubernetes deployment"""
 
+# Uses some equations from 'Practical Efficient Microservice Autoscaling' (Md Rajib Hossen, Mohammad A. Islam)
+
 import os
 import threading
 import time
@@ -23,11 +25,13 @@ PROM_CPU_UTILISATION = f"http://{IP_ADDRESS}:{PROM_PORT}/api/v1/query?query=cont
 PROM_CPU_THROTTLING = f"http://{IP_ADDRESS}:{PROM_PORT}/api/v1/query?query=container_cpu_cfs_throttled_seconds_total{{namespace='socialnetwork', pod=~'.*service.*'}}[1m]"
 
 # SCALING SPECIFIC SETTINGS
-SCALE_DOWN_THRESHOLD_UTILISATION = 45.0  # cpu utilisation % when you need to scale down
-SCALE_DOWN_THRESHOLD_THROTTLING = 2.5  # cpu throttle % when you need to scale down
+SCALE_DOWN_THRESHOLD_RESPONSE_TIME = 300 # response time in microseconds when scaling down is needed
+SCALE_DOWN_THRESHOLD_UTILISATION = 45.0  # cpu utilisation % when scaling down is needed
+SCALE_DOWN_THRESHOLD_THROTTLING = 2.5  # cpu throttle % when scaling down is needed
 SCALE_DOWN_GRACE_PERIOD = 15  # time in seconds between first meeting the threshold and then scaling down
-SCALE_UP_THRESHOLD_UTILISATION = 85.0  # cpu utilisation % when you need to scale up
-SCALE_UP_THRESHOLD_THROTTLING = 10.0  # cpu throttling % when you need to scale up
+SCALE_UP_THRESHOLD_RESPONSE_TIME = 1000 # response time in microseconds when scaling up is needed
+SCALE_UP_THRESHOLD_UTILISATION = 85.0  # cpu utilisation % when scaling up is needed
+SCALE_UP_THRESHOLD_THROTTLING = 10.0  # cpu throttling % when scaling up is needed
 SCALE_UP_GRACE_PERIOD = 5  # time in seconds between first meeting the threshold and then scaling up
 AFTER_GRACE_PERIOD = 15  # time in seconds after scaling to wait before scaling again
 MINIMUM_INSTANCES = 1  # min amount of instances of each service
@@ -44,17 +48,17 @@ running_threads = []
 
 # DICTIONARY OF SERVICE INFORMATION
 services = {
-    "post-storage-service": {"response_time": [0.0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
-    "user-mention-service": {"response_time": [0.0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
-    "user-service": {"response_time": [0.0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
-    "unique-id-service": {"response_time": [0.0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
-    "media-service": {"response_time": [0.0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
-    "social-graph-service": {"response_time": [0.0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
-    "url-shorten-service": {"response_time": [0.0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
-    "compose-post-service": {"response_time": [0.0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
-    "user-timeline-service": {"response_time": [0.0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
-    "home-timeline-service": {"response_time": [0.0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
-    "text-service": {"response_time": [0.0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1}}
+    "post-storage-service": {"response_time": [0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
+    "user-mention-service": {"response_time": [0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
+    "user-service": {"response_time": [0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
+    "unique-id-service": {"response_time": [0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
+    "media-service": {"response_time": [0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
+    "social-graph-service": {"response_time": [0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
+    "url-shorten-service": {"response_time": [0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
+    "compose-post-service": {"response_time": [0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
+    "user-timeline-service": {"response_time": [0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
+    "home-timeline-service": {"response_time": [0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1},
+    "text-service": {"response_time": [0], "cpu_utilisation": [0.0], "cpu_throttling": [0.0], "instances": 1}}
 
 
 def main():
@@ -183,38 +187,42 @@ def autoscale():
                 cpu_throttling = float(values[-1][1])  # get most recent value
                 services[service_name]['cpu_throttling'] = add_to_list(cpu_throttling, services[service_name]['cpu_throttling'])
 
-            print("--------------------------------------------------")
+            print("---------------------------------------------------------------------")
+            print("SERVICE NAME               | UTILISATION | THROTTLING | RESPONSE TIME")
+            print("---------------------------------------------------------------------")
+
             for service_name, service_data in services.items():
                 instances = service_data["instances"]
                 cpu_util = float(service_data['cpu_utilisation'][0])
                 cpu_throttling = float(service_data['cpu_throttling'][0])
-                response_time = float(service_data["response_time"][0])
+                response_time = service_data["response_time"][0]
 
-                # print the data into a table
-                print(f"{service_name:<{22}}"
-                      f"| INSTANCES: {f'{instances}x':<{3}}"
-                      f"| UTILISATION: {f'{cpu_util:.2f}%':<{8}}"
-                      f"| THROTTLING: {f'{cpu_throttling:.2f}%':<{8}}"
-                      f"| RESPONSE TIME: {f'{response_time:.2f}µs':<{10}}")
+                # print the data into the table
+                print(f"{service_name:<{21}} ({instances}x) "
+                      f"| {f'{cpu_util:.2f}%':<{12}}"
+                      f"| {f'{cpu_throttling:.2f}%':<{11}}"
+                      f"| {f'{response_time}µs':<{16}}")
 
                 # check if you need to scale the service
                 # based on whether you are allowed to add or remove instances
                 # and whether they are already attempting to scale
                 if service_data['instances'] < MAXIMUM_INSTANCES and service_name not in running_threads:
-                    max_cpu_util = SCALE_UP_THRESHOLD_UTILISATION * service_data['instances']
-
-                    should_scale_up = (cpu_util > max_cpu_util or cpu_throttling > SCALE_UP_THRESHOLD_THROTTLING)
+                    should_scale_up = (response_time > SCALE_UP_THRESHOLD_RESPONSE_TIME 
+                                       or cpu_util > SCALE_UP_THRESHOLD_UTILISATION 
+                                       or cpu_throttling > SCALE_UP_THRESHOLD_THROTTLING)
 
                     if should_scale_up:
                         thread = threading.Thread(target=scale_up, args=(service_name,), name=f"{service_name}")
                         running_threads.append(f"{service_name}")
                         thread.start()
                 elif service_data['instances'] > MINIMUM_INSTANCES and service_name not in running_threads:
-                    should_scale_down = (
-                            cpu_util < SCALE_DOWN_THRESHOLD_UTILISATION and cpu_throttling < SCALE_DOWN_THRESHOLD_THROTTLING)
+                    should_scale_down = (response_time < SCALE_DOWN_THRESHOLD_RESPONSE_TIME 
+                                         and cpu_util < SCALE_DOWN_THRESHOLD_UTILISATION 
+                                         and cpu_throttling < SCALE_DOWN_THRESHOLD_THROTTLING)
 
                     if should_scale_down:
-                        thread = threading.Thread(target=scale_down, args=(service_name,), name=f"{service_name}")
+                        reduction = (service_data['instances'] - 1) * min((SCALE_DOWN_THRESHOLD_RESPONSE_TIME - service_data['response_time']) / SCALE_DOWN_THRESHOLD_RESPONSE_TIME, 1)
+                        thread = threading.Thread(target=scale_down, args=(service_name,reduction,), name=f"{service_name}")
                         running_threads.append(f"{service_name}")
                         thread.start()
 
@@ -237,10 +245,15 @@ def scale_up(service_name):
     """Scales up the given service"""
     time.sleep(SCALE_UP_GRACE_PERIOD)  # check after grace period before scaling
 
+    response_time = services[service_name]['response_time'][0]
     cpu_util = float(services[service_name]['cpu_utilisation'][0])
     cpu_throttling = float(services[service_name]['cpu_throttling'][0])
 
-    if cpu_util > SCALE_UP_THRESHOLD_UTILISATION or cpu_throttling > SCALE_UP_THRESHOLD_THROTTLING:
+    still_exceeding_threshold = (response_time > SCALE_UP_THRESHOLD_RESPONSE_TIME 
+                                 or cpu_util > SCALE_UP_THRESHOLD_UTILISATION 
+                                 or cpu_throttling > SCALE_UP_THRESHOLD_THROTTLING)
+
+    if still_exceeding_threshold:
         # read namespace
         deployment = apps_api.read_namespaced_deployment(name=service_name, namespace='socialnetwork')
         deployment.spec.replicas = services[service_name]['instances'] + 1
@@ -255,22 +268,27 @@ def scale_up(service_name):
     running_threads.remove(f"{service_name}")  # remove thread from list so that it can run again
 
 
-def scale_down(service_name):
+def scale_down(service_name, reduction):
     """Scales down the given service"""
     time.sleep(SCALE_DOWN_GRACE_PERIOD)  # check after grace period before scaling
 
+    response_time = services[service_name]['response_time'][0]
     cpu_util = float(services[service_name]['cpu_utilisation'][0])
     cpu_throttling = float(services[service_name]['cpu_throttling'][0])
 
-    if cpu_util < SCALE_DOWN_THRESHOLD_UTILISATION and cpu_throttling < SCALE_DOWN_THRESHOLD_THROTTLING:
+    still_under_threshold = (response_time < SCALE_DOWN_THRESHOLD_RESPONSE_TIME 
+                                 and cpu_util < SCALE_DOWN_THRESHOLD_UTILISATION 
+                                 and cpu_throttling < SCALE_DOWN_THRESHOLD_THROTTLING)
+
+    if still_under_threshold:
         # read namespace
         deployment = apps_api.read_namespaced_deployment(name=service_name, namespace='socialnetwork')
-        deployment.spec.replicas = services[service_name]['instances'] - 1
+        deployment.spec.replicas = services[service_name]['instances'] - reduction
 
         # patch namespace
         apps_api.patch_namespaced_deployment(name=service_name, namespace='socialnetwork', body=deployment)
 
-        services[service_name]['instances'] -= 1  # update the number of instances in the service list
+        services[service_name]['instances'] -= reduction  # update the number of instances in the service list
         print(f"↓ {service_name} scaled DOWN to {services[service_name]['instances']} instance(s)")
         time.sleep(AFTER_GRACE_PERIOD)  # wait before allowing to scale up or down again
 
@@ -279,34 +297,3 @@ def scale_down(service_name):
 
 if __name__ == "__main__":
     main()
-
-# TODO: Implement efficient scaling algorithm 
-#       from 'Practical Efficient Microservice Autoscaling' (Md Rajib Hossen, Mohammad A. Islam)
-#
-# step 1:   for each time step:
-#
-# step 2:   grab response time, CPU utilisation and CPU throttling statistics
-#
-# step 3:   log the performance statistics for future reference
-#
-# step 4:   check if the response times are too slow and exceed defined limits, 
-#           allocate more resources to ensure that the performance is back to acceptable range
-#           (then go back to step 1)
-#
-# step 5:   recalculate safe limits for CPU usage and throttling (using equation 6 and 7 
-#           from the paper)
-#
-# step 6:   calculate exploration probability (equation 8) to decide whether or not to try 
-#           a previously stable resource allocation configuration (then go back to step 1)
-#
-# step 7:   determine a new target resource allocation (reduction) based on a reduction limit
-#           (i.e. do not scale down more than 20%) (using equation 3 and 4 from the paper)
-#
-# step 8:   identify services that are currently throttling or close to utilisation limits,
-#           and ensure these services are excluded from resource reductions
-#
-# step 9:   assign higher probability of reduction to the services that have a lower utilisation,
-#           ensuring a more safe approach to scaling down (using equation 5 from the paper)
-#
-# step 10:  apply step 7 reduction target, ignoring bottleneck services and then uniformly
-#           reduce the resources across all non-bottleneck services for next time step
