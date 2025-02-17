a58b65607044ada656a109692a8644a09ebf5459
Lewis <lewisryework@gmail.com>
2025-02-17 13:38:25 +0000

    Update how chaosd is installed

--- socialNetwork/install_chaosd.sh
+++ socialNetwork/install_chaosd.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # download tar.gz
-curl -fsSLO https://mirrors.chaos-mesh.org/chaosd-v1.4.0-linux-amd64.tar.gz
-tar zxvf chaosd-v1.4.0-linux-amd64.tar.gz && sudo mv chaosd-v1.4.0-linux-amd64 /usr/local/
+curl -fsSL -o chaosd-v1.4.0-linux-amd64.tar.gz https://mirrors.chaos-mesh.org/chaosd-v1.4.0-linux-amd64.tar.gz
+tar zxvf chaosd-v1.4.0-linux-amd64.tar.gz && sudo mv chaosd-v1.4.0-linux-amd64 /usr/local/chaosd
 
 # update PATH
-echo 'export PATH=$PATH:/usr/local/chaosd-v1.4.0-linux-amd64' >> ~/.bashrc
+echo 'export PATH=$PATH:/usr/local/chaosd/chaosd-v1.4.0-linux-amd64' >> ~/.bashrc
 source ~/.bashrc
 
 # cleanup
