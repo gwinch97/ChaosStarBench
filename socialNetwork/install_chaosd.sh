ecc4a4199859f78f990f680bea1d4bc0b59ca7fe
Lewis <lewisryework@gmail.com>
2025-02-18 09:53:06 +0000

    Easier install for chaosd

--- socialNetwork/install_chaosd.sh
+++ socialNetwork/install_chaosd.sh
@@ -2,12 +2,15 @@
 
 # download tar.gz
 curl -fsSL -o chaosd-v1.4.0-linux-amd64.tar.gz https://mirrors.chaos-mesh.org/chaosd-v1.4.0-linux-amd64.tar.gz
-tar zxvf chaosd-v1.4.0-linux-amd64.tar.gz && sudo mv chaosd-v1.4.0-linux-amd64 /usr/local/chaosd
 
-# update PATH
-echo 'export PATH=$PATH:/usr/local/chaosd/chaosd-v1.4.0-linux-amd64' >> ~/.bashrc
-source ~/.bashrc
+# unzip tar.gz
+tar zxvf chaosd-v1.4.0-linux-amd64.tar.gz && cd chaosd-v1.4.0-linux-amd64/
 
-# cleanup
-rm chaosd-v1.4.0-linux-amd64.tar.gz
-echo "Success: chaosd installed successfully."
+# install to user's system
+sudo install -o root -g root -m 0755 chaosd /usr/local/bin/chaosd
+
+if command -v chaosd &>/dev/null; then
+    echo "Success: Chaosd installed successfully."
+else
+    echo "Error: Chaosd was NOT installed successfully."
+fi
