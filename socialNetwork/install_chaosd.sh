9ab312fe72e8b64f754630afafb75f3f6232a5f4
Lewis <lewisryework@gmail.com>
2025-02-19 10:38:58 +0000

    Install all required chaosd tools for experiments

--- socialNetwork/install_chaosd.sh
+++ socialNetwork/install_chaosd.sh
@@ -6,11 +6,22 @@ curl -fsSL -o chaosd-v1.4.0-linux-amd64.tar.gz https://mirrors.chaos-mesh.org/ch
 # unzip tar.gz
 tar zxvf chaosd-v1.4.0-linux-amd64.tar.gz && cd chaosd-v1.4.0-linux-amd64/
 
-# install to user's system
+# install all tools to user's system (except byteman - this project does not need JVM chaos)
 sudo install -o root -g root -m 0755 chaosd /usr/local/bin/chaosd
+cd tools/
+sudo install -o root -g root -m 0755 FileTool /usr/local/bin/FileTool
+sudo install -o root -g root -m 0755 memStress /usr/local/bin/memStress
+sudo install -o root -g root -m 0755 PortOccupyTool /usr/local/bin/PortOccupyTool
+sudo install -o root -g root -m 0755 stress-ng /usr/local/bin/stress-ng
+sudo install -o root -g root -m 0755 tproxy /usr/local/bin/tproxy
+
+# cleanup
+cd ../../
+rm -rf chaosd-v1.4.0-linux-amd64/
+rm chaosd-v1.4.0-linux-amd64.tar.gz
 
 if command -v chaosd &>/dev/null; then
-    echo "Success: Chaosd installed successfully."
+    echo "Success: Chaosd tools installed successfully."
 else
     echo "Error: Chaosd was NOT installed successfully."
 fi
